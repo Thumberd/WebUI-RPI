e8fc18f9a868aa03449fbf6c20bb6cb5b742303c
root <ajeremyalbrecht@gmail.com>
2016-08-19 22:46:06 +0200

    Change DB

---
+++ database/migrations/2016_08_19_210805_create_data_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('datas', function (Blueprint $table) {
+            $table->increments('id');
+		$table->integer('data_type');
+		$table->double('value');
+		$table->integer('device_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('datas');
+    }
+}
